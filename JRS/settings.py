47f5f028c1d3e00376f760e7ad9d644c2b429151
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2023-12-02 15:44:06 +0500

    settings.py

--- JRS/settings.py
+++ JRS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y^u20z4-my9*%xx3v@5-5bdesc_%bptw^=%z3audo=#0+y-=4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.vercel.app]
 
 
 # Application definition
